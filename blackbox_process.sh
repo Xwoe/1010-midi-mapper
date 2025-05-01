669e8c05dea59a1147b49b230c673e4b9b7b0356
Xwoe <plongaaa@gmail.com>
2025-05-01 15:17:03 +0200

    add tests and blackbox_process.sh

---
+++ blackbox_process.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Parse command-line arguments
+while getopts "i:o:r" opt; do
+  case $opt in
+    i) infile="$OPTARG" ;;
+    o) outfolder="$OPTARG" ;;
+    r) overwrite="--overwrite" ;;
+    *) echo "Usage: $0 -i <input_file> -o <output_folder> [-r]"; exit 1 ;;
+  esac
+done
+
+# Check if required arguments are provided
+if [ -z "$infile" ] || [ -z "$outfolder" ]; then
+  echo "Usage: $0 -i <input_file> -o <output_folder> [-r]"
+  exit 1
+fi
+
+# Call the midi_mapper.py script with the provided arguments
+python3 midi_mapper.py -i "$infile" -o "$outfolder" -t blackbox $overwrite
